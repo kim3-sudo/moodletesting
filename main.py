126393ce7ceb1890a8ad4ae3f4603e7f90c418f3
Sejin Kim <kim3@kenyon.edu>
2022-02-13 18:32:47 -0500

    Add text edition and updated file capabilities

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import json
 import random
 import threading
@@ -5,6 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 def generate(users, courses):
@@ -47,14 +50,12 @@ def login(uri, driver, username, password):
     """
     driver.implicitly_wait(3)
     driver.get(uri)
-    LoginXpath = '//*[@id="page-wrapper"]/nav/ul[2]/li[2]/div/span/a'
-    driver.find_element(By.XPATH, LoginXpath).click()
+    driver.find_element(By.XPATH, '//*[@id="page-wrapper"]/nav/ul[2]/li[2]/div/span/a').click()
     driver.find_element(By.ID, "username").send_keys(username)
     driver.find_element(By.ID, "password").send_keys(password)
     driver.find_element(By.ID, "loginbtn").click()
     try:
-        NextXpath = '/html/body/span/div/div/div[4]/button[3]'
-        driver.find_element(By.XPATH, NextXpath).click()
+        driver.find_element(By.XPATH, '/html/body/span/div/div/div[4]/button[3]').click()
         print("Tutorial modal found and dismissed")
     except:
         print("No tutorial modal")
@@ -73,10 +74,8 @@ def logout(uri, driver):
     None
     """
     print("Logging out user")
-    UsermenuXpath = "//a[contains(@aria-label, 'User menu')]"
-    driver.find_element(By.XPATH, UsermenuXpath).click()
-    LogoutXpath = "//a[contains(@href, 'logout.php')]"
-    driver.find_element(By.XPATH, LogoutXpath).click()
+    driver.find_element(By.XPATH, "//a[contains(@aria-label, 'User menu')]").click()
+    driver.find_element(By.XPATH, "//a[contains(@href, 'logout.php')]").click()
 
 def rng1(driver, course):
     """
@@ -95,11 +94,9 @@ def rng1(driver, course):
 
     """
     print("RNG 1: Access course")
-    CourseXpath = "//a[contains(@href, 'course/view.php?id=" + course + "')]"
-    driver.find_element(By.XPATH, CourseXpath).click()
+    driver.find_element(By.XPATH, "//a[contains(@href, 'course/view.php?id=" + course + "')]").click()
     try:
-        TutorialXpath = "/html/body/span/div/div/div[4]/button[3]"
-        driver.find_element(By.XPATH, TutorialXpath).click()
+        driver.find_element(By.XPATH, "/html/body/span/div/div/div[4]/button[3]").click()
         print("Course tutorial modal found and dismissed")
     except:
         print("No tutorial modal")
@@ -124,8 +121,7 @@ def rng2(driver, uri):
     userid = str(random.choice(range(2, 1000)))
     driver.get(uri + "/user/view.php?id=" + userid)
     try:
-        TutorialXpath = "/html/body/span/div/div/div[4]/button[3]"
-        driver.find_element(By.XPATH, TutorialXpath).click()
+        driver.find_element(By.XPATH, "/html/body/span/div/div/div[4]/button[3]").click()
         print("Course tutorial modal found and dismissed")
     except:
         print("No tutorial modal")
@@ -152,11 +148,82 @@ def rng3(driver, uri, course):
     userid = str(random.choice(range(2, 1000)))
     driver.get(uri + "/user/view.php?id=" + userid + "&course=" + course)
     try:
-        TutorialXpath = "/html/body/span/div/div/div[4]/button[3]"
-        driver.find_element(By.XPATH, TutorialXpath).click()
+        driver.find_element(By.XPATH, "/html/body/span/div/div/div[4]/button[3]").click()
         print("Course tutorial modal found and dismissed")
     except:
         print("No tutorial modal")
+
+def rng4(driver, uri):
+    """
+    Accesses a random user's profile (ID between 2, 1000) and submits Essay.odt on their behalf
+
+    Parameters
+    ----------
+    driver : Selenium webdriver
+        A webdriver object to control browser with
+    uri : string
+        The host URI
+
+    Return
+    ------
+    None
+
+    """
+    print("Getting course 9")
+    driver.find_element(By.XPATH, "//a[contains(@href, 'course/view.php?id=9')]").click()
+    print("On course ID 9")
+    driver.implicitly_wait(2)
+    try:
+        driver.find_element(By.XPATH, "/html/body/span/div/div/div[4]/button[3]").click()
+        print("Course tutorial modal found and dismissed")
+    except:
+        print("No tutorial modal")
+    driver.implicitly_wait(2)
+    print("Selecting assignment")
+    driver.find_element(By.XPATH, "//a[contains(@href, 'assign/view.php?id=140')]").click()
+    driver.implicitly_wait(2)
+    try:
+        print("Trying to remove old submission")
+        driver.find_element(By.XPATH, "//button[contains(text(), 'Remove submission')]").click()
+        driver.implicitly_wait(0.5)
+        driver.find_element(By.XPATH, "//button[contains(text(), 'Continue')]").click()
+        driver.implicitly_wait(0.5)
+        driver.find_element(By.XPATH, "//button[contains(text(), 'Edit submission')]").click()
+    except:
+        print("Unable to remove old submission (maybe it doesn't exist). Adding instead.")
+        print("Selecting Add submission button")
+        driver.find_element(By.XPATH, "//button[contains(text(), 'Add submission')]").click()
+    driver.implicitly_wait(0.5)
+    submissiontype = random.choice([True, False]) # True is online text, false is upload
+    #submissiontype = True
+    if submissiontype == True:
+        print("Uploading in web editor")
+        essay = ""
+        with open('./essay.txt', 'r') as f:
+            essay = f.read()
+        print("Essay preview: " + essay[0:50])
+        driver.find_element(By.ID, "id_onlinetext_editoreditable").click()
+        actions = ActionChains(driver)
+        actions.send_keys(essay)
+        actions.perform()
+        #driver.find_element(By.ID, "id_onlinetext_editor").send_keys(essay)
+        #driver.find_element(By.ID, "id_onlinetext_editor").send_keys(essay)
+    else:
+        print("Uploading as document")
+        essay = "./Essay.odt"
+        driver.find_element(By.XPATH, "//a[contains(@title, 'Add...')]").click()
+        driver.implicitly_wait(1)
+        driver.find_element(By.XPATH, "//span[contains(text(), 'Upload a file')]").click()
+        driver.implicitly_wait(1)
+        print("Sending essay string")
+        driver.find_element(By.XPATH, "//input[@type='file']").send_keys(os.getcwd() + "/" + essay)
+        print("Essay string sent")
+        print("Uploading file")
+        driver.find_element(By.XPATH, "//button[contains(text(), 'Upload this file')]").click()
+    driver.implicitly_wait(2)
+    print("Submitting essay")
+    driver.find_element(By.XPATH, "//input[contains(@name, 'submitbutton')]").click()
+    print("Essay submitted")
 
 def simaction(threads, driver, service, options, uri, users, courses, password):
     try:
@@ -164,15 +231,19 @@ def simaction(threads, driver, service, options, uri, users, courses, password):
         username, course = generate(users, courses)
         login(uri, driver, username, password)
         driver.implicitly_wait(5)
-        rngaction = random.choice(list(range(1, 3)))
-        #rngaction = 3
-        print("RNG action" + str(rngaction))
+        rngaction = random.choice(list(range(1,4)))
+        #rngaction = 4
+        print("RNG action " + str(rngaction))
         if (rngaction == 1):
             rng1(driver, course)
         elif (rngaction == 2):
             rng2(driver, uri)
-        else:
+        elif (rngaction == 3):
             rng3(driver, uri, course)
+        elif (rngaction == 4):
+            rng4(driver, uri)
+        else:
+            rng1(driver, course)
         logout(uri, driver)
         print("Destroying driver")
         driver.quit()
